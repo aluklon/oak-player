244b098d25e46312c32829108b0b37f3696186ec
Alexey Lukin <aluklon@gmail.com>
2021-01-15 17:24:36 +0300

    IMPR: Added player thread.
    Change-Id: Ibd9772db30dccb26240680f523d19605f5ee8b5a

--- src/player.cpp
+++ src/player.cpp
@@ -1,19 +1,38 @@
 #include "player.h"
 
+// C++ headers
+#include <thread>
+
 // Local headers
+#include "common/event_loop.h"
 #include "common/http/http_client.h"
 
 namespace oak {
 
+static void ThreadFunc(EventLoop* loop) {
+    loop->run();
+}
+
 class Player::Impl {
 public:
+    Impl();
+
     void load(const std::string& url);
+
+    EventLoop _loop;
+    std::thread _thread;
 
     http::HttpClient _httpClient;
 };
 
+Player::Impl::Impl()
+    : _thread(ThreadFunc, &_loop) {
+}
+
 void Player::Impl::load(const std::string &url) {
-    _httpClient.get(http::Request(url));
+    _loop.dispatch([this, url] () {
+        _httpClient.get(http::Request(url));
+    });
 }
 
 Player::Player()
