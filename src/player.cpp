35543afc19370f1ea945ffd5fb670f4c93b2c98e
Alexey Lukin <aluklon@gmail.com>
2021-02-19 20:42:42 +0300

    FIX: Correct stopping of player thread.
    Change-Id: I9c1053c853d46a92ad1ea050013970083062b403

--- src/player.cpp
+++ src/player.cpp
@@ -9,28 +9,33 @@
 
 namespace oak {
 
-static void ThreadFunc(EventLoop* loop) {
-    loop->run();
-}
-
 class Player::Impl {
 public:
     Impl();
+    ~Impl();
 
     void load(const std::string& url);
 
-    EventLoop _loop;
+    EventLoop* _loop = nullptr;
     std::thread _thread;
 
     http::HttpClient _httpClient;
 };
 
 Player::Impl::Impl()
-    : _thread(ThreadFunc, &_loop) {
+    : _loop(new EventLoop())
+    , _thread(&EventLoop::run, std::ref(_loop)) {
+}
+
+Player::Impl::~Impl() {
+    _loop->dispatch([this] () {
+        delete _loop;
+    });
+    _thread.join();
 }
 
 void Player::Impl::load(const std::string &url) {
-    _loop.dispatch([this, url] () {
+    _loop->dispatch([this, url] () {
         _httpClient.get(http::Request(url));
     });
 }
